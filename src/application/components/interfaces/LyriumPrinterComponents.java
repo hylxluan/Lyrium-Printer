38434adc151b8526273aed52a9e9c5e45fd6b4cd
Luan <luanplays212004@gmail.com>
2025-03-27 00:16:08 -0300

    Finished left side of Printer UI buttons

--- src/application/components/interfaces/LyriumPrinterComponents.java
+++ src/application/components/interfaces/LyriumPrinterComponents.java
@@ -7,4 +7,11 @@ public interface LyriumPrinterComponents {
 	void initializeUi(Stage stage);
 	
 	void createSearchButton();
+	
+	void createPrintDocButton();
+	
+	void createHeadAlignmentButton();
+	
+	void createNozzleCheckButton();
+	
 }
